7410755ea5bc94228c1088075bfe7c7e35983259
GnanendraKumarNalliboina <gnanendra.nalliboina@bluepal.com>
2024-02-22 16:15:25 +0530

    Basic GetMapping is added in controller file...!!!!..

--- src/main/java/com/Employee/EmployeeTask/Controller/EmployeeController.java
+++ src/main/java/com/Employee/EmployeeTask/Controller/EmployeeController.java
@@ -1,5 +1,12 @@
 package com.Employee.EmployeeTask.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class EmployeeController {
     
+    @GetMapping("/aws")
+    public ResponseEntity<String> health(){
+        return ResponseEntity.ok("spring boot application is running on Amazon EC2 Instance!!!!....");
+    }
 }
