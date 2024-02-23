bc5f0cb81bc9fecc21cc18aff6c0b03837a24e98
Eesa <shaik.easa@bluepal.com>
2024-02-23 17:54:34 +0530

    add some thing controller

--- src/main/java/com/Employee/EmployeeTask/Controller/EmployeeController.java
+++ src/main/java/com/Employee/EmployeeTask/Controller/EmployeeController.java
@@ -10,4 +10,8 @@ public class EmployeeController {
     public String helloworld(){
         return ("spring boot application is running on Amazon EC2 Instance!!!!....");
     }
+    @GetMapping("/aws")
+    public String helloworld1(){
+        return ("spring boot application is running on Amazon EC2 Instance!!!!....");
+    }
 }
